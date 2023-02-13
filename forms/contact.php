0cf3e0716fb89034b2753de7eca763003e8b99ac
santidevhmo <santiagoagdc42@gmail.com>
2023-02-13 12:07:06 -0700

    Removed phpInfo()

--- forms/contact.php
+++ forms/contact.php
@@ -37,8 +37,6 @@
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
-  phpinfo();
   
   echo $contact->send();
 ?>
